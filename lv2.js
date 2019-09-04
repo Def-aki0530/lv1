be483219579a67578cc53c7ecd6cd4f98d1eb056
anzai_kouki <54619761+Def-aki0530@users.noreply.github.com>
2019-09-04 22:33:30 +0900

    Add files via upload

---
+++ lv2.js
@@ -0,0 +1,32 @@
+$(function(){
+    $('.hello_html').addClass('show');
+    $('.hello_js').addClass('hide');
+    var check=0;
+    document.getElementById("btn").addEventListener("click", function(){
+        if(check == 0){
+            $('.hello_html').removeClass('show');
+            $('.hello_html').addClass('hide');
+            $('.hello_js').removeClass('hide');
+            $('.hello_js').addClass('show');
+            check = 1;
+        }else{
+            $('.hello_html').removeClass('hide');
+            $('.hello_html').addClass('show');
+            $('.hello_js').removeClass('show');
+            $('.hello_js').addClass('hide');
+            check = 0;
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+});
